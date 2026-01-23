b220204bec899779624ab7f19b7cf4f88d68b263
tivvvv <18612553935@163.com>
2026-01-23 09:14:39 +0800

    feat: 基于Spring Schedule定时抓取股票信息.

---
+++ stock-monitor-web/src/main/java/com/tiv/stock/monitor/web/schedule/StockSchedule.java
@@ -0,0 +1,21 @@
+package com.tiv.stock.monitor.web.schedule;
+
+import com.tiv.stock.monitor.web.service.RssService;
+import jakarta.annotation.Resource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+public class StockSchedule {
+
+    @Resource
+    private RssService rssService;
+
+    @Scheduled(cron = "*/30 * * * * ?")
+    public void fetchStockInfo() {
+        rssService.displayRss();
+    }
+
+}
