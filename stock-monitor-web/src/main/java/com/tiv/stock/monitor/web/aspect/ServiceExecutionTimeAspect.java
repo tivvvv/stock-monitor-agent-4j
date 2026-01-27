214fe78cc3242289681471f9dd102372679fba9e
tivvvv <18612553935@163.com>
2026-01-27 12:49:07 +0800

    perf: 基于StopWatch优化服务执行时间监控实现.

--- stock-monitor-web/src/main/java/com/tiv/stock/monitor/web/aspect/ServiceExecutionTimeAspect.java
+++ stock-monitor-web/src/main/java/com/tiv/stock/monitor/web/aspect/ServiceExecutionTimeAspect.java
@@ -5,6 +5,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 /**
  * 服务执行时间切面
@@ -17,21 +18,21 @@ public class ServiceExecutionTimeAspect {
     @Around("execution(* com.tiv.stock.monitor.web.service.impl..*.*(..))")
     public Object recordTimeLog(ProceedingJoinPoint joinPoint) throws Throwable {
 
-        long beginTime = System.currentTimeMillis();
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
 
         Object proceed = joinPoint.proceed();
         String pointCut = joinPoint.getTarget().getClass().getName() + "." + joinPoint.getSignature().getName();
 
-        long endTime = System.currentTimeMillis();
-
-        long executeTime = endTime - beginTime;
+        stopWatch.stop();
+        long executeTime = stopWatch.getTotalTimeMillis();
 
         if (executeTime > 5000) {
-            log.error("SLOW: {} took {}ms, args: {}", pointCut, executeTime, joinPoint.getArgs());
+            log.error("SLOW: {} took {} ms, args: {}", pointCut, executeTime, joinPoint.getArgs());
         } else if (executeTime > 2000) {
-            log.warn("MODERATE: {} took {}ms, args: {}", pointCut, executeTime, joinPoint.getArgs());
+            log.warn("MODERATE: {} took {} ms, args: {}", pointCut, executeTime, joinPoint.getArgs());
         } else {
-            log.debug("FAST: {} took {}ms", pointCut, executeTime);
+            log.debug("FAST: {} took {} ms, args: {}", pointCut, executeTime, joinPoint.getArgs());
         }
         return proceed;
     }
