74378849a703291fd053e6addc5016a28b7630b1
tivvvv <18612553935@163.com>
2026-02-07 17:18:18 +0800

    feat: 使用.env文件配置环境参数.

--- stock-monitor-mcp/src/main/java/com/tiv/stock/monitor/mcp/McpApplication.java
+++ stock-monitor-mcp/src/main/java/com/tiv/stock/monitor/mcp/McpApplication.java
@@ -1,5 +1,6 @@
 package com.tiv.stock.monitor.mcp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -9,6 +10,10 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 public class McpApplication {
 
     public static void main(String[] args) {
+        // 加载.env文件
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        // 把.env文件中的变量参数设置到当前项目中
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(McpApplication.class, args);
     }
 
